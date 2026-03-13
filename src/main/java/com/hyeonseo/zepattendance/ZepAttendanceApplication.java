59d796c9d88922bee601fde3ca259acd416838c1
hyeonseo1109 <alsgustj2@naver.com>
2026-03-13 19:50:55 +0900

    Initial commit

---
+++ src/main/java/com/hyeonseo/zepattendance/ZepAttendanceApplication.java
@@ -0,0 +1,13 @@
+package com.hyeonseo.zepattendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZepAttendanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZepAttendanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyeonseo/zepattendance/ZepAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeonseo.zepattendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZepAttendanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
