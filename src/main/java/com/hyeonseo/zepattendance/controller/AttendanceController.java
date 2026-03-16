2236a42a5b997ef21b8e9b980ff5ac175468de96
hyeonseo1109 <alsgustj2@naver.com>
2026-03-16 23:25:26 +0900

    refactor: 출석 문구 Service 코드에서 관리하도록 의존성 분리

--- src/main/java/com/hyeonseo/zepattendance/controller/AttendanceController.java
+++ src/main/java/com/hyeonseo/zepattendance/controller/AttendanceController.java
@@ -16,17 +16,6 @@ public class AttendanceController {
 
     private final AttendanceService attendanceService;
 
-    private static final Random random = new Random();
-
-    private static final String[] MESSAGES = {
-            "어머 이게 웬일이야~ 반가워라!",
-            "학생~ 또 왔구려~ 홀홀홀..",
-            "어이! 지각이다 지각! 운동장 5바퀴 돌고 와!",
-            "후후.. 기다리고 있었습니다, 이쪽으로 모시지요.",
-            "너,.너 누구야! 여기 어떻게 들어왔어!",
-            "흥.. 절대 기다린 건 아니라구."
-    };
-
     @PostMapping
     public AttendanceResponseDto checkAttendance(
             @RequestBody AttendanceRequestDto request
@@ -34,7 +23,7 @@ public class AttendanceController {
 
         Attendance attendance = attendanceService.checkAttendance(request);
 
-        String message =  MESSAGES[random.nextInt(MESSAGES.length)];
+        String message = attendanceService.getRandomMessage();
 
         return new AttendanceResponseDto(
                 attendance.getId(),
--- src/main/java/com/hyeonseo/zepattendance/service/AttendanceService.java
+++ src/main/java/com/hyeonseo/zepattendance/service/AttendanceService.java
@@ -8,11 +8,23 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class AttendanceService {
     private final AttendanceRepository attendanceRepository;
+
+    private static final Random random = new Random();
+
+    private static final String[] MESSAGES = {
+            "어머 이게 웬일이야~ 반가워라!",
+            "학생~ 또 왔구려~ 홀홀홀..",
+            "어이! 지각이다 지각! 운동장 5바퀴 돌고 와!",
+            "후후.. 기다리고 있었습니다, 이쪽으로 모시지요.",
+            "너,.너 누구야! 여기 어떻게 들어왔어!",
+            "흥.. 절대 기다린 건 아니라구."
+    };
 
     public Attendance checkAttendance(AttendanceRequestDto request) {
         LocalDate today = LocalDate.now(ZoneId.of("Asia/Seoul"));
@@ -29,5 +41,8 @@ public class AttendanceService {
         );
 
         return attendanceRepository.save(attendance);
+    }
+    public String getRandomMessage() {
+        return MESSAGES[random.nextInt(MESSAGES.length)];
     }
 }
