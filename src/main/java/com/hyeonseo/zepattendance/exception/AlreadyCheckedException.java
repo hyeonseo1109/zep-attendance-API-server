a10e7a0577f13397b8a77b58412092b8979538df
hyeonseo1109 <alsgustj2@naver.com>
2026-03-17 02:29:52 +0900

    refactor: 예외처리 문구 변경

--- src/main/java/com/hyeonseo/zepattendance/exception/AlreadyCheckedException.java
+++ src/main/java/com/hyeonseo/zepattendance/exception/AlreadyCheckedException.java
@@ -2,6 +2,6 @@ package com.hyeonseo.zepattendance.exception;
 
 public class AlreadyCheckedException extends RuntimeException {
     public AlreadyCheckedException() {
-        super("이미 출석했습니다.");
+        super("이미 출석했자나~~");
     }
 }
